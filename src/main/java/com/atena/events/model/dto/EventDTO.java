a7ab283c7c510500caa58b85b88aff4ead8080c0
ryanfurt12 <ryanfurtadinho9@gmail.com>
2025-11-26 19:39:45 -0300

    HOT FIX AAAA

--- src/main/java/com/atena/events/model/dto/EventDTO.java
+++ src/main/java/com/atena/events/model/dto/EventDTO.java
@@ -1,6 +1,7 @@
 package com.atena.events.model.dto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +28,9 @@ public class EventDTO {
         this.date = event.getDate();
         this.ownerId = event.getOwner().getId();
         this.ownerName = event.getOwner().getName();
-        this.participantsIds = event.getParticipants().stream()
+        this.participantsIds = (event.getParticipants() == null)
+        ? new ArrayList<>()
+        : event.getParticipants().stream()
             .filter(p -> p != null && p.getId() != null)
             .map(p -> p.getId().toString())
             .collect(Collectors.toList());
