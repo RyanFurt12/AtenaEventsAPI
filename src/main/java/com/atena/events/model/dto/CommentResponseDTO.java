132a3aee3fb89bb66c2cd596f712a0f8188a88cb
ryanfurt12 <ryanfurtadinho9@gmail.com>
2025-11-27 17:44:37 -0300

    feat:add comment author id

--- src/main/java/com/atena/events/model/dto/CommentResponseDTO.java
+++ src/main/java/com/atena/events/model/dto/CommentResponseDTO.java
@@ -8,12 +8,16 @@ import lombok.Data;
 
 @Data
 public class CommentResponseDTO {
+    private Long id;
     private String text;
+    private Long authorId;
     private String authorName;
     private LocalDateTime createdAt;
 
     public CommentResponseDTO(Comment c) {
+        this.id = c.getId();
         this.text = c.getText();
+        this.authorId = c.getAuthor().getId();
         this.authorName = c.getAuthor().getName();
         this.createdAt = c.getCreatedAt();
     }
